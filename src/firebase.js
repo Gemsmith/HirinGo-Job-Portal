9ad9b18c5a01e5e3a2960f38ccd080c0b3b937d7
Gemsmith <gemsmith101@gmail.com>
2022-07-08 20:34:57 +0530

    Update firebase.js
    Removed API Keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCPyF0rFLQ8Vd-7oEU_jakXZKAM5vly4Ow',
-  authDomain: 'hiringo-cv-uploads.firebaseapp.com',
-  projectId: 'hiringo-cv-uploads',
-  storageBucket: 'hiringo-cv-uploads.appspot.com',
-  messagingSenderId: '413404417366',
-  appId: '1:413404417366:web:2e6d79643dc9458f3a1af7',
+  apiKey: 'ADD_YOUR_FIREBASE_DATA_HERE',
+  authDomain: 'ADD_YOUR_FIREBASE_DATA_HERE',
+  projectId: 'ADD_YOUR_FIREBASE_DATA_HERE',
+  storageBucket: 'ADD_YOUR_FIREBASE_DATA_HERE',
+  messagingSenderId: 'ADD_YOUR_FIREBASE_DATA_HERE',
+  appId: 'ADD_YOUR_FIREBASE_DATA_HERE',
 };
 
 // Initialize Firebase
